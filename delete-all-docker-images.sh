d0c3f9949479f7e2f6a41cb71497a3f45d8c3fbb
Marton Bur <marton.bur@gmail.com>
2018-10-23 12:51:17 -0400

    Initial commit of productivity scripts

---
+++ delete-all-docker-images.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# source: https://techoverflow.net/2013/10/22/docker-remove-all-images-and-containers/
+
+# Delete all containers
+docker rm $(docker ps -a -q)
+# Delete all images
+docker rmi $(docker images -q)
+
---
+++ set-ifmetrics.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+while true
+do
+
+ifmetric enp0s25 5
+
+ifmetric wlp4s0 0
+
+sleep 60
+
+done
